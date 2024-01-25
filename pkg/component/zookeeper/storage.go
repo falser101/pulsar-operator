03b6e612cc4818eec8c76a04d60ee45523210da0
T2816 <zhangjf@tongtech.com>
2024-01-25 15:11:46 +0800

    fix: zookeeper sc

--- pkg/component/zookeeper/storage.go
+++ pkg/component/zookeeper/storage.go
@@ -9,7 +9,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func makePVCName(c *v1alpha1.Pulsar) string {
+func makeZookeeperDateVolumeName(c *v1alpha1.Pulsar) string {
 	return fmt.Sprintf("%s-zookeeper-data", c.Name)
 }
 
@@ -17,28 +17,26 @@ func makeVolumeClaimTemplates(c *v1alpha1.Pulsar) []v1.PersistentVolumeClaim {
 	return []v1.PersistentVolumeClaim{
 		{
 			ObjectMeta: metav1.ObjectMeta{
-				Name: makePVCName(c),
+				Name: makeZookeeperDateVolumeName(c),
 			},
-			Spec: v1.PersistentVolumeClaimSpec{
-				AccessModes: []v1.PersistentVolumeAccessMode{
-					v1.ReadWriteOnce,
-				},
-				Resources: v1.ResourceRequirements{
-					Requests: v1.ResourceList{
-						v1.ResourceStorage: resource.MustParse("10Gi"),
-					},
-				},
-				StorageClassName: &c.Spec.Zookeeper.StorageClassName,
-				VolumeName:       "Filesystem",
-			},
+			Spec: makeZookeeperDataVolumeClaimSpec(c),
 		},
+	}
+}
+
+func makeZookeeperDataVolumeClaimSpec(c *v1alpha1.Pulsar) v1.PersistentVolumeClaimSpec {
+	capacity := fmt.Sprintf("%dGi", c.Spec.Zookeeper.StorageCapacity)
+	return v1.PersistentVolumeClaimSpec{
+		AccessModes:      []v1.PersistentVolumeAccessMode{v1.ReadWriteOnce},
+		Resources:        v1.ResourceRequirements{Requests: v1.ResourceList{v1.ResourceStorage: resource.MustParse(capacity)}},
+		StorageClassName: &c.Spec.Zookeeper.StorageClassName,
 	}
 }
 
 func makeEmptyDirVolume(c *v1alpha1.Pulsar) []v1.Volume {
 	return []v1.Volume{
 		{
-			Name: ContainerDataVolumeName,
+			Name: makeZookeeperDateVolumeName(c),
 			VolumeSource: v1.VolumeSource{
 				EmptyDir: &v1.EmptyDirVolumeSource{},
 			},
