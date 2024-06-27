c4056b6b6f09a42f8bb0f5e43bb5c6ef50daf2f2
falser <zjf_0731@163.com>
2024-06-27 11:05:27 +0800

    feat: 增加zk 的rbac

---
+++ internal/component/zookeeper/rbac.go
@@ -0,0 +1,73 @@
+package zookeeper
+
+import (
+	"fmt"
+	"github.com/falser101/pulsar-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func MakeServiceAccount(c *v1alpha1.PulsarCluster) *corev1.ServiceAccount {
+	return &corev1.ServiceAccount{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "v1",
+			Kind:       "ServiceAccount",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      makeServiceAccountName(c),
+			Namespace: c.Namespace,
+			Labels:    v1alpha1.MakeComponentLabels(c, v1alpha1.ZookeeperComponent),
+		},
+	}
+}
+
+func makeServiceAccountName(c *v1alpha1.PulsarCluster) string {
+	return fmt.Sprintf("%s-%s", c.Name, v1alpha1.ZookeeperComponent)
+}
+
+func MakeRole(c *v1alpha1.PulsarCluster) *rbacv1.Role {
+	return &rbacv1.Role{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "rbac.authorization.k8s.io/v1",
+			Kind:       "Role",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "zookeeper",
+			Namespace: c.Namespace,
+		},
+		Rules: []rbacv1.PolicyRule{
+			{
+				APIGroups:     []string{"policy"},
+				Resources:     []string{"podsecuritypolicies"},
+				ResourceNames: []string{},
+				Verbs:         []string{"use"},
+			},
+		},
+	}
+}
+
+func MakeRoleBinding(c *v1alpha1.PulsarCluster) *rbacv1.RoleBinding {
+	return &rbacv1.RoleBinding{
+		TypeMeta: metav1.TypeMeta{
+			APIVersion: "rbac.authorization.k8s.io/v1",
+			Kind:       "RoleBinding",
+		},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "zookeeper",
+			Namespace: c.Namespace,
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "Role",
+			Name:     "zookeeper",
+		},
+		Subjects: []rbacv1.Subject{
+			{
+				Kind:      "ServiceAccount",
+				Name:      "zookeeper",
+				Namespace: c.Namespace,
+			},
+		},
+	}
+}
